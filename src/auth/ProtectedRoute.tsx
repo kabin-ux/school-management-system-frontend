27bef7cd82a1a69f4e806def90304fe9f3cc47d7
kabin-ux <dongolkabin@gmail.com>
2025-10-26 09:56:53 +0545

    token expire fix

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuthUser } from "../hooks/useAuth";
+import { jwtDecode } from "jwt-decode";
 
 interface ProtectedRouteProps {
     children: React.ReactNode;
     allowedRoles: string[]; // roles allowed for this route
 }
 
+interface DecodedToken {
+    exp: number;
+}
+
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children, allowedRoles }) => {
     const { data: user, isLoading } = useAuthUser();
     const role = localStorage.getItem("role");
+    const token = localStorage.getItem("token");
 
     if (isLoading) {
         return <div>Loading...</div>
@@ -20,6 +26,20 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children, allowedRoles 
         return <Navigate to="/admin" replace />;
     }
 
+    try {
+        const decoded = jwtDecode<DecodedToken>(token || "");
+        const currentTime = Date.now() / 1000;
+
+        if (decoded.exp < currentTime) {
+            localStorage.clear();
+
+            return <Navigate to="/admin" replace />;
+        }
+    } catch (error) {
+        console.error("Invalid token", error)
+        localStorage.clear();
+        return <Navigate to="/admin" replace />;
+    }
 
     if (!role || !allowedRoles.includes(role)) {
         // Logged in but role not allowed → redirect to unauthorized page or login
